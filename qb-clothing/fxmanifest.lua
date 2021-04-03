b2db8a09fd6fd31413f991e46084125c10d6ff01
GhzGarage <57848836+GhzGarage@users.noreply.github.com>
2021-04-03 01:08:39 -0500

    fxmanifest

--- qb-clothing/fxmanifest.lua
+++ qb-clothing/fxmanifest.lua
@@ -1,22 +1,28 @@
-resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+fx_version 'cerulean'
+game 'gta5'
 
-description 'QB Clothing'
+description 'QB-Clothing'
+version '1.0.0'
 
 ui_page "html/index.html"
 
 server_scripts {
 	"config.lua",
-	"server/main.lua",
+	"server/main.lua"
 }
 
 client_scripts {
 	"config.lua",
-	"client/main.lua",
+	"client/main.lua"
 }
 
 files {
 	'html/index.html',
 	'html/style.css',
 	'html/reset.css',
-	'html/script.js',
+	'html/script.js'
+}
+
+dependencies {
+	'qb-core'
 }
