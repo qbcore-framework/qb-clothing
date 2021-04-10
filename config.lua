958e8eb32099cc60382f2f075b9c7930f0e4c777
GhzGarage <qbcore@programmer.net>
2021-04-09 22:05:52 -0500

    Coord Changes

--- config.lua
+++ config.lua
@@ -507,15 +507,28 @@ Config.LoadedManModels = {}
 Config.LoadedWomanModels = {}
 
 Config.Stores = {
-    [1] =   {shopType = "clothing", x = 260.46,  y = 1290.90, z = 14.76},
-	[2] =   {shopType = "clothing", x = -46.27,  y = 1857.96, z = 24.63},
-	[3] =   {shopType = "clothing", x = 248.90,  y = -176.28, z = 14.86},
-	[4] =   {shopType = "clothing", x =  1406.80,     y = 2161.05,    z = 16.77},
-	[5] =   {shopType = "clothing", x = -1321.12,     y = 496.63,    z = 9.05},
-	[6] =  {shopType = "barber",   x = 19.15,       y = 1848.21,      z = 21.75},
-	[7] =  {shopType = "barber",   x = -686.61,        y = 1726.61,     z = 24.98},
-	[8] =  {shopType = "surgeon",   x = 1464.81,      y = 688.79,     z = 33.51}, 
-	[9] =   {shopType = "clothing", x = 1118.32,     y = 50.35,    z = 15.85},
+    [1] =   {shopType = "clothing", x = 1693.32,      y = 4823.48,     z = 41.06},
+	[2] =   {shopType = "clothing", x = -712.215881,  y = -155.352982, z = 37.4151268},
+	[3] =   {shopType = "clothing", x = -1192.94495,  y = -772.688965, z = 17.3255997},
+	[4] =   {shopType = "clothing", x =  425.236,     y = -806.008,    z = 28.491},
+	[5] =   {shopType = "clothing", x = -162.658,     y = -303.397,    z = 38.733},
+	[6] =   {shopType = "clothing", x = 75.950,       y = -1392.891,   z = 28.376},
+	[7] =   {shopType = "clothing", x = -822.194,     y = -1074.134,   z = 10.328},
+	[8] =   {shopType = "clothing", x = -1450.711,    y = -236.83,     z = 48.809},
+	[9] =   {shopType = "clothing", x = 4.254,        y = 6512.813,    z = 30.877},
+	[10] =  {shopType = "clothing", x = 615.180,      y = 2762.933,    z = 41.088},
+	[11] =  {shopType = "clothing", x = 1196.785,     y = 2709.558,    z = 37.222},
+	[12] =  {shopType = "clothing", x = -3171.453,    y = 1043.857,    z = 19.863},
+	[13] =  {shopType = "clothing", x = -1100.959,    y = 2710.211,    z = 18.107},
+	[14] =  {shopType = "clothing", x = -1207.65,     y = -1456.88,    z = 4.3784737586975},
+    [15] =  {shopType = "clothing", x = 121.76,       y = -224.6,      z = 53.56},
+	[16] =  {shopType = "barber",   x = -814.3,       y = -183.8,      z = 36.6},
+	[17] =  {shopType = "barber",   x = 136.8,        y = -1708.4,     z = 28.3},
+	[18] =  {shopType = "barber",   x = -1282.6,      y = -1116.8,     z = 6.0},
+	[19] =  {shopType = "barber",   x = 1931.5,       y = 3729.7,      z = 31.8},
+	[20] =  {shopType = "barber",   x = 1212.8,       y = -472.9,      z = 65.2},
+	[21] =  {shopType = "barber",   x = -32.9,        y = -152.3,      z = 56.1},
+	[22] =  {shopType = "barber",   x = -278.1,       y = 6228.5,      z = 30.7}
 }
 
 Config.ClothingRooms = {
