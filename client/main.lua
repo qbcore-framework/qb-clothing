c0e3187568fe71615afc2489b880a4eb85057518
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-06-27 02:06:10 +0200

    Tables correction

--- client/main.lua
+++ client/main.lua
@@ -384,11 +384,11 @@ Citizen.CreateThread(function()
                         DrawMarker(2, Config.Stores[k].coords.x,Config.Stores[k].coords.y,Config.Stores[k].coords.z + 0.98, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.4, 0.2, 255, 255, 255, 255, 0, 0, 0, 1, 0, 0, 0)
                         if dist < 5 then
                             if Config.Stores[k].shopType == "clothing" then
-                                 DrawText3Ds((Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25), '~g~E~w~ - To Shop For Clothes')
+                                 DrawText3Ds({Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25}, '~g~E~w~ - To Shop For Clothes')
                             elseif Config.Stores[k].shopType == "barber" then
-                                DrawText3Ds((Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25), '~g~E~w~ - To Get A Haircut')
+                                DrawText3Ds({Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25}, '~g~E~w~ - To Get A Haircut')
                             elseif Config.Stores[k].shopType == "surgeon" then
-                                DrawText3Ds((Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25), '~g~E~w~ - To Get Plastic Surgery')
+                                DrawText3Ds({Config.Stores[k].coords.x, Config.Stores[k].coords.y, Config.Stores[k].coords.z + 1.25}, '~g~E~w~ - To Get Plastic Surgery')
                             end
                             if IsControlJustPressed(0, 38) then -- E
                                 if Config.Stores[k].shopType == "clothing" then
@@ -443,7 +443,7 @@ Citizen.CreateThread(function()
                         DrawMarker(2, Config.ClothingRooms[k].coords, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.4, 0.4, 0.2, 255, 255, 255, 255, 0, 0, 0, 1, 0, 0, 0)
                         if dist < 2 then
                             if PlayerData.job.name == Config.ClothingRooms[k].requiredJob then
-                                DrawText3Ds((Config.ClothingRooms[k].coords.x, Config.ClothingRooms[k].coords.y, Config.ClothingRooms[k].coords.z + 0.3), '~g~E~w~ - View Clothing')
+                                DrawText3Ds({Config.ClothingRooms[k].coords.x, Config.ClothingRooms[k].coords.y, Config.ClothingRooms[k].coords.z + 0.3}, '~g~E~w~ - View Clothing')
                                 if IsControlJustPressed(0, 38) then -- E
                                     customCamLocation = Config.ClothingRooms[k][3].cameraLocation
                                     gender = "male"
