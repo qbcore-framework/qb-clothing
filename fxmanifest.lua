3f2cc23fe529b1ab454b6fbdbcf16fcb0634759d
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2021-10-20 13:39:58 +0200

    chore(manifest): Removed import.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,10 +6,7 @@ version '1.0.0'
 
 ui_page 'html/index.html'
 
-shared_scripts { 
-	'@qb-core/import.lua',
-	'config.lua'
-}
+shared_script 'config.lua'
 
 server_script 'server/main.lua'
 client_script 'client/main.lua'
@@ -21,6 +18,4 @@ files {
 	'html/script.js'
 }
 
-dependencies {
-	'qb-core'
-}
+dependency 'qb-core'
