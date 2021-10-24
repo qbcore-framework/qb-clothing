5289c709abf3e0c6c9a207799aefad50d532756d
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2021-10-24 11:18:39 -0600

    chore(oxmysql): Replace fetch

--- server/main.lua
+++ server/main.lua
@@ -19,7 +19,7 @@ RegisterServerEvent("qb-clothes:loadPlayerSkin")
 AddEventHandler('qb-clothes:loadPlayerSkin', function()
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local result = exports.oxmysql:fetchSync('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', { Player.PlayerData.citizenid, 1 })
+    local result = exports.oxmysql:executeSync('SELECT * FROM playerskins WHERE citizenid = ? AND active = ?', { Player.PlayerData.citizenid, 1 })
     if result[1] ~= nil then
         TriggerClientEvent("qb-clothes:loadSkin", src, false, result[1].model, result[1].skin)
     else
@@ -40,7 +40,7 @@ AddEventHandler("qb-clothes:saveOutfit", function(outfitName, model, skinData)
             json.encode(skinData),
             outfitId
         }, function()
-            local result = exports.oxmysql:fetchSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
+            local result = exports.oxmysql:executeSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
             if result[1] ~= nil then
                 TriggerClientEvent('qb-clothing:client:reloadOutfits', src, result)
             else
@@ -59,7 +59,7 @@ AddEventHandler("qb-clothing:server:removeOutfit", function(outfitName, outfitId
         outfitName,
         outfitId
     }, function()
-        local result = exports.oxmysql:fetchSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
+        local result = exports.oxmysql:executeSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
         if result[1] ~= nil then
             TriggerClientEvent('qb-clothing:client:reloadOutfits', src, result)
         else
@@ -73,7 +73,7 @@ QBCore.Functions.CreateCallback('qb-clothing:server:getOutfits', function(source
     local Player = QBCore.Functions.GetPlayer(src)
     local anusVal = {}
 
-    local result = exports.oxmysql:fetchSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
+    local result = exports.oxmysql:executeSync('SELECT * FROM player_outfits WHERE citizenid = ?', { Player.PlayerData.citizenid })
     if result[1] ~= nil then
         for k, v in pairs(result) do
             result[k].skin = json.decode(result[k].skin)
