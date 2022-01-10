2deff157b4f6160adaa3729bcd1c1cb959e3f91d
Kieran Dyer <kierandyer@airsorted.com>
2022-01-10 16:52:47 +0000

    Import MYSQL to Resource

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,10 @@ version '1.0.0'
 ui_page 'html/index.html'
 
 shared_script 'config.lua'
-server_script 'server/main.lua'
+server_script {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
 client_script 'client/main.lua'
 
 files {
