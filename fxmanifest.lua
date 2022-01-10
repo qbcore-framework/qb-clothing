a0f078ae63e0ac858d88d74205bb9a6510d910c9
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2022-01-10 11:59:36 -0600

    Oxmysql

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,7 +7,12 @@ version '1.0.0'
 ui_page 'html/index.html'
 
 shared_script 'config.lua'
-server_script 'server/main.lua'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
 client_script 'client/main.lua'
 
 files {
