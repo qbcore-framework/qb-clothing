df58f43664d172caf7e55a2fba3b4354312d5d6f
jnccloud <58707473+jnccloud@users.noreply.github.com>
2022-12-19 09:25:21 -0500

    Update qb-clothing.sql
    Co-authored-by: Zerio <54480523+Z3rio@users.noreply.github.com>

--- qb-clothing.sql
+++ qb-clothing.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS `playerskins` (
   PRIMARY KEY (`id`),
   KEY `citizenid` (`citizenid`),
   KEY `active` (`active`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `player_outfits` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
